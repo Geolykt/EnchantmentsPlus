3c48b334f6855b8d826d0c99ac8182c1cd75a01c
Geolykt <admin@geolykt.de>
2020-09-07 22:40:55 +0200

    The color of the particles of the Laser can now be changed
    Why? Don't ask. I was bored.

--- src/main/java/de/geolykt/enchantments_plus/util/ColUtil.java
+++ src/main/java/de/geolykt/enchantments_plus/util/ColUtil.java
@@ -1,6 +1,7 @@
 package de.geolykt.enchantments_plus.util;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
 import org.bukkit.DyeColor;
 import org.bukkit.Material;
 
@@ -1124,6 +1125,48 @@ public class ColUtil {
             return Material.YELLOW_WOOL;
         default:
             return Material.WHITE_WOOL;
+        }
+    }
+
+    public static Color toBukkitColor(String parseString, Color defaultValue) {
+        switch (parseString) {
+        case "AQUA":
+            return Color.AQUA;
+        case "BLACK":
+            return Color.BLACK;
+        case "BLUE":
+            return Color.BLUE;
+        case "FUCHSIA":
+            return Color.FUCHSIA;
+        case "GRAY":
+        case "GREY":
+            return Color.GRAY;
+        case "GREEN":
+            return Color.GREEN;
+        case "LIME":
+            return Color.LIME;
+        case "MAROON":
+            return Color.MAROON;
+        case "NAVY":
+            return Color.NAVY;
+        case "OLIVE":
+            return Color.OLIVE;
+        case "ORANGE":
+            return Color.ORANGE;
+        case "PURPLE":
+            return Color.PURPLE;
+        case "RED":
+            return Color.RED;
+        case "SILVER":
+            return Color.SILVER;
+        case "TEAL":
+            return Color.TEAL;
+        case "WHITE": 
+            return Color.WHITE;
+        case "YELLOW":
+            return Color.YELLOW;
+        default:
+            return defaultValue;
         }
     }
     
